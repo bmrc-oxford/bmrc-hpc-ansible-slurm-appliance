de090eb2d07e09ada186c9006525b64f688b4bc6
Steve Brasier <steveb@stackhpc.com>
2025-07-31 12:40:44 +0000

    changes to node/cluster names

--- environments/production/tofu/main.tf
+++ environments/production/tofu/main.tf
@@ -6,12 +6,12 @@ variable "environment_root" {
 module "cluster" {
   source = "../../site/tofu/"
 
-  cluster_name = "nvs"
-  cluster_nodename_template = "$${node}.$${cluster_domain_suffix}"
+  cluster_name = "ms"
+  cluster_nodename_template = "ms-$${node}.$${cluster_domain_suffix}"
 
   login = {
     interactive = {
-        nodes = ["login-0"]
+        nodes = ["login-00"]
         flavor = "m1.highmem"
         fip_addresses = ["10.167.2.160"]
     }
@@ -19,17 +19,12 @@ module "cluster" {
 
   compute = {
     general = {
-      nodes = ["compute-0", "compute-1"]
-      flavor = "m2.large"
+      nodes = ["general-00", "general-01"]
+      flavor = "m2.xlarge.highmem_200disk"
     }
-  }
-
-
-  additional_nodegroups = {
-    squid = { # node for EESSI squid proxy
-      nodes = ["eessi-squid-0"]
-      flavor = "m1.large" # docs state few cores + few GB of RAM required
-      fip_addresses = ["10.167.2.160"]
+    legacy = {
+      nodes = ["legacy-00", "legacy-01"]
+      flavor = "m3.medium"
     }
   }
 
