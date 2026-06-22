10865a226725f45fd50b658329ba06f0533f3342
Steve Brasier <steveb@stackhpc.com>
2026-06-22 12:44:57 +0000

    Resized m3.large.100disk to m3.large.icelake.100disk for ms-general-[09-15]

--- environments/production/tofu/main.tf
+++ environments/production/tofu/main.tf
@@ -31,7 +31,7 @@ module "cluster" {
     general-100disk = {
       nodes = ["general-09", "general-10", "general-11", "general-12",
                "general-13", "general-14", "general-15"]
-      flavor = "m3.large.100disk"
+      flavor = "m3.large.icelake.100disk"
     }
     legacy = {
       nodes = ["legacy-00", "legacy-01"]
