5ad7ad7cdd9173cf7593f42220dca4a0b7e54128
Eric Le Lay <eric@stackhpc.com>
2026-07-01 15:47:02 +0200

    Retrieve ansible-vault password from hashicorp vault

--- dev/env-vault-client.py
+++ dev/env-vault-client.py
@@ -7,21 +7,19 @@ import sys
 def main():
 
   parser = argparse.ArgumentParser(description='Get ansible vault password from environment')
-  
   parser.add_argument('--vault-id', required=True, help='name of the vault secret (VAULT_<SECRET>_PASSWORD env var)')
-  
-  
+
   args = parser.parse_args()
-  
+
   secret = args.vault_id.upper()
-  
+
   env_name = f"VAULT_{secret}_PASSWORD"
   secret = os.environ.get(env_name)
 
   if not secret:
       print(f"E: env variable {env_name} is not set", file=sys.stderr)
       return 1
-  
+
   sys.stdout.write(f"{secret}\n")
 
 
