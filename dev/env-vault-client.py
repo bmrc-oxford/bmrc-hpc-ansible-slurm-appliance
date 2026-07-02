2ef770f59297c8d1118c1eb739473dbbf0c6d09e
Eric Le Lay <eric@stackhpc.com>
2026-07-02 10:18:49 +0200

    Fix linter warnings

--- dev/env-vault-client.py
+++ dev/env-vault-client.py
@@ -4,24 +4,31 @@ import argparse
 import os
 import sys
 
+
 def main():
 
-  parser = argparse.ArgumentParser(description='Get ansible vault password from environment')
-  parser.add_argument('--vault-id', required=True, help='name of the vault secret (VAULT_<SECRET>_PASSWORD env var)')
+    parser = argparse.ArgumentParser(
+        description="Get ansible vault password from environment"
+    )
+    parser.add_argument(
+        "--vault-id",
+        required=True,
+        help="name of the vault secret (VAULT_<SECRET>_PASSWORD env var)",
+    )
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  secret = args.vault_id.upper()
+    secret = args.vault_id.upper()
 
-  env_name = f"VAULT_{secret}_PASSWORD"
-  secret = os.environ.get(env_name)
+    env_name = f"VAULT_{secret}_PASSWORD"
+    secret = os.environ.get(env_name)
 
-  if not secret:
-      print(f"E: env variable {env_name} is not set", file=sys.stderr)
-      return 1
+    if not secret:
+        print(f"E: env variable {env_name} is not set", file=sys.stderr)
+        return 1
 
-  sys.stdout.write(f"{secret}\n")
+    sys.stdout.write(f"{secret}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
--- environments/site/README.md
+++ environments/site/README.md
@@ -2,4 +2,4 @@
 
 BMRC hpc site work in progress
 
-See the main README.md in the repo root for an overview and general install instructions.  Any environment-specific instructions should be added here.
+See the main README.md in the repository root for an overview and general install instructions. Any environment-specific instructions should be added here.
